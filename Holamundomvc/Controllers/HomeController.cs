96b02b8904b578ff88a47c6b7c878416a2a1b9b2
Mario Nares <mario_nares@hotmail.com>
2023-03-28 19:25:25 -0700

    Agregar archivos de proyecto.

---
+++ Holamundomvc/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Holamundomvc.Models;
+
+namespace Holamundomvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new HelloWorldModel { message = "Hola mundo" };
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Holamundomvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Holamundomvc.MvcApplication" Language="C#" %>
